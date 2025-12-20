9fb6afac20e5164379f6613c6a054b4b72181d42
SahinKavsara <kavsara12@gmail.com>
2025-12-20 19:27:43 +0300

    AuthService rotalari users yerine api olarak duzeltildi

--- Frontend/mekanbul-frontend/src/services/AuthService.jsx
+++ Frontend/mekanbul-frontend/src/services/AuthService.jsx
@@ -2,12 +2,12 @@ import http from "./http-common";
 
 const register = (data) => {
   // Backend'de yol '/users/register' olduğu için burayı güncelledik
-  return http.post("/users/register", data);
+  return http.post("/api/register", data);
 };
 
 const login = (data) => {
   // Backend'de yol '/users/login' olduğu için burayı güncelledik
-  return http.post("/users/login", data);
+  return http.post("/api/login", data);
 };
 
 const AuthService = {
